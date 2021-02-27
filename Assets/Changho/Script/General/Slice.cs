deb973bfc99da61743498e282dbc1758453ddfb8
sochangho <ckdgh940@naver.com>
2021-02-27 23:08:02 +0900

    메쉬 채우기 , 폴더 생성

--- Assets/Changho/Script/General/Slice.cs
+++ Assets/Changho/Script/General/Slice.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Slice : MonoBehaviour
 {
 
-
+    public Material mt;
 
 
     /// <summary>
@@ -425,7 +425,25 @@ public class Slice : MonoBehaviour
         List<Vector3> sorted_createVerts;
         SortVertices(new_vertics, out sorted_createVerts);
 
+        List<Vector3> aSideCutVerts;
+        List<Vector3> bSideCutVerts;
 
+        List<Vector3> aSideCutNormals;
+        List<Vector3> bSideCutNormals;
+
+        List<Vector2> aSideCutUVs;
+        List<Vector2> bSideCutUVs;
+
+        List<int> aSideCutT;
+        List<int> bSideCutT;
+
+        MakeCutSide(random_plane.normal, sorted_createVerts,
+            out aSideCutVerts, out bSideCutVerts, out aSideCutNormals,
+            out bSideCutNormals, out aSideCutUVs, out bSideCutUVs,
+            out aSideCutT , out bSideCutT
+            );
+
+  
         Mesh aMesh = new Mesh();
 
         Mesh bMesh = new Mesh();
@@ -438,8 +456,9 @@ public class Slice : MonoBehaviour
         aMesh.normals = slide_normalsA.ToArray();
 
         aMesh.uv = slide_uvsA.ToArray();
-
+        aMesh.subMeshCount = target.GetComponent<MeshRenderer>().sharedMaterials.Length + 1;
         aMesh.SetTriangles(slide_triA, 0);
+        aMesh.SetTriangles(aSideCutT, target.GetComponent<MeshRenderer>().sharedMaterials.Length);
 
 
 
@@ -452,9 +471,9 @@ public class Slice : MonoBehaviour
         bMesh.normals = slide_normalsB.ToArray();
 
         bMesh.uv = slide_uvsB.ToArray();
-
+        bMesh.subMeshCount = target.GetComponent<MeshRenderer>().sharedMaterials.Length + 1;
         bMesh.SetTriangles(slide_triB, 0);
-
+        bMesh.SetTriangles(bSideCutT, target.GetComponent<MeshRenderer>().sharedMaterials.Length);
 
 
 
@@ -468,53 +487,35 @@ public class Slice : MonoBehaviour
 
 
 
-        Material[] mats = target.GetComponent<MeshRenderer>().sharedMaterials;
+        Material[] mats = new Material[target.GetComponent<MeshRenderer>().sharedMaterials.Length + 1];
 
 
-
-
+        for (int i = 0; i < target.GetComponent<MeshRenderer>().sharedMaterials.Length; i++)
+        {
+            mats[i] = target.GetComponent<MeshRenderer>().sharedMaterials[i];
+        }
+        mats[target.GetComponent<MeshRenderer>().sharedMaterials.Length] = material;
         aObject.GetComponent<MeshFilter>().sharedMesh = aMesh;
-
         aObject.GetComponent<MeshRenderer>().sharedMaterials = mats;
-
-
-
-
         bObject.GetComponent<MeshFilter>().sharedMesh = bMesh;
-
         bObject.GetComponent<MeshRenderer>().sharedMaterials = mats;
-
-
-
-
         aObject.transform.position = target.transform.position;
-
         aObject.transform.rotation = target.transform.rotation;
-
         aObject.transform.localScale = target.transform.localScale;
-
         bObject.transform.position = target.transform.position;
-
         bObject.transform.rotation = target.transform.rotation;
-
         bObject.transform.localScale = target.transform.localScale;
 
-
-
-
-
-
-
         target.SetActive(false);
-
-
-
-
         return new GameObject[] { aObject, bObject };
 
     }
 
-
+    /// <summary>
+    /// new verts를 정렬
+    /// </summary>
+    /// <param name="verts">new verts </param>
+    /// <param name="result"> 정렬된 결과  </param>
     public void SortVertices(List<Vector3> verts , out List<Vector3> result)
     {
         result = new List<Vector3>();
@@ -536,19 +537,159 @@ public class Slice : MonoBehaviour
                 if(vert1 == cVert0)
                 {
                     result.Add(cVert1);
-                    
+
+                    SwapTwoIndexSet<Vector3>(ref verts, i * 2 + 2, i * 2 + 3, j * 2, j * 2 + 1);
+                }
+                else if(vert1 == cVert1){
+
+                    result.Add(cVert0);
+
+                    SwapTwoIndexSet<Vector3>(ref verts, i * 2 + 2, i * 2 + 3, j * 2 + 1, j * 2);
+
                 }
 
             }
 
         }
 
+        if (result[0] == result[result.Count - 1])
+        {
+            result.RemoveAt(result.Count - 1);
+        }
     }
 
+    /// <summary>
+    /// 자르고 난 후에 단면을 만든다 단면의 가운데에 새로운 정점을 만들고 새로운 정점과 잘린 단면의 정점들을 이어준다
+    /// </summary>
+    /// <param name="normal">잘린 단면의 법선 벡터</param>
+    /// <param name="sortedCV">정렬된 벡터</param>
+    /// <param name="aSCV">A새로운 정점의 위치</param>
+    /// <param name="bSCV">B새로운 정점의 위치</param>
+    /// <param name="aSCN">A새로운 점의 법선 벡터</param>
+    /// <param name="bSCN">B새로운 점의 법선</param>
+    /// <param name="aSCU">A새로운 uv</param>
+    /// <param name="bSCU">B새로운 uv</param>
+    /// <param name="aSCT">A의 새로운 폴리곤</param>
+    /// <param name="bSCT">B의 새로운 폴리곤 삼각형</param>
+   void MakeCutSide(Vector3 normal,List<Vector3> sortedCV,
+          out List<Vector3> aSCV,out List<Vector3>  bSCV,out List<Vector3> aSCN,
+          out List<Vector3> bSCN,out List<Vector2> aSCU,out List<Vector2> bSCU,
+          out List<int> aSCT , out List<int> bSCT
+          )
+    {
+        aSCV = new List<Vector3>();
+        bSCV = new List<Vector3>();
+        aSCN = new List<Vector3>();
+        bSCN = new List<Vector3>();
+        aSCU = new List<Vector2>();
+        bSCU = new List<Vector2>();
+        aSCT = new List<int>();
+        bSCT = new List<int>();
+        aSCV.AddRange(sortedCV);
+        bSCV.AddRange(sortedCV);
+
+        if(sortedCV.Count < 2) return;
+
+        Vector3 center = Vector3.zero;
+
+        foreach(Vector3 v in sortedCV)
+        {
+            center += v;
+        }
+
+        center /= sortedCV.Count;
+
+        aSCV.Add(center);
+        bSCV.Add(center);
+
+        for(int i =0; i < aSCV.Count; i++)
+        {
+            aSCN.Add(normal);           
+        }
+
+        for(int i = 0; i< bSCV.Count; i++)
+        {
+            bSCN.Add(normal);
+        }
+
+
+        Vector3 forward = Vector3.zero;
+
+        forward.x = normal.y;
+        forward.y = normal.x;
+        forward.z = normal.z;
+
+
+        Vector3 left = Vector3.Cross(forward, normal);
+
+        for(int i = 0; i < sortedCV.Count; i++)
+        {
+            Vector3 dir = sortedCV[i] - center;
+
+            Vector2 relateUV = Vector2.zero;
+
+            relateUV.x = 0.5f + Vector3.Dot(dir, left);
+            relateUV.y = 0.5f + Vector3.Dot(dir, forward);
+
+            aSCU.Add(relateUV);
+            bSCU.Add(relateUV);
+        }
+
+        aSCU.Add(new Vector2(0.5f, 0.5f));
+        bSCU.Add(new Vector2(0.5f, 0.5f));
+
+
+        int centerIdx = aSCV.Count - 1;
+
+        float normalDir = Vector3.Dot(normal, Vector3.Cross(sortedCV[0] - center, sortedCV[1] - sortedCV[0]));
+
+        for(int i = 0; i< aSCV.Count; i++)
+        {
+            int idx0 = i;
+            int idx1 = (i + 1) % (aSCV.Count - 1);
+
+
+            if (normalDir < 0)
+            {
+                aSCT.Add(centerIdx);
+                aSCT.Add(idx1);
+                aSCT.Add(idx0);
+
+                bSCT.Add(centerIdx);
+                bSCT.Add(idx0);
+                bSCT.Add(idx1);
+
+            }
+            else
+            {
+                aSCT.Add(centerIdx);
+                aSCT.Add(idx0);
+                aSCT.Add(idx1);
+
+                bSCT.Add(centerIdx);
+                bSCT.Add(idx1);
+                bSCT.Add(idx0);
+            }
+
+
+
+        }
+
+
+
+    }
 
     void SwapTwoIndexSet<T>(ref List<T> verts , int index00 , int index01, int index10, int index11 )
     {
+        T temp0 = verts[index00];
+        T temp1 = verts[index01];
 
+        verts[index00] = verts[index10];
+        verts[index01] = verts[index11];
+
+
+        verts[index10] = temp0;
+        verts[index11] = temp1;
     }
 
 
@@ -558,7 +699,7 @@ public class Slice : MonoBehaviour
     {
 
 
-       // Slicer(this.gameObject);
+       Slicer(this.gameObject , mt);
 
     }
 
